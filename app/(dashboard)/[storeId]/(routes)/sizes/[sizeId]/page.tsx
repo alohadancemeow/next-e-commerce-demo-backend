69409ad0842e95ee33f298258e7fcfcee08eb390
Alohadancemeow <nawongsak@gmail.com>
2023-10-04 15:54:47 +0700

    🍀 add size page

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/page.tsx
@@ -0,0 +1,32 @@
+import prismadb from "@/lib/prismadb";
+
+import { SizeForm } from "./components/size-form";
+
+const SizePage = async ({ params }: { params: { sizeId: string } }) => {
+  const getSize = async () => {
+    try {
+      const size = await prismadb.size.findUnique({
+        where: {
+          id: params.sizeId,
+        },
+      });
+
+      return size;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  const size = await getSize();
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 p-8 pt-6 space-y-4">
+        <SizeForm initialData={size} />
+      </div>
+    </div>
+  );
+};
+
+export default SizePage;
